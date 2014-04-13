9ee101161d02351a947ded6c08b75126df9e42eb
Martijn de Milliano <mjdemilliano@gmail.com>
2014-04-13 17:55:47 +0200

    Calculating probability density function and performing word selection based on that, so that difficult words have a higher probability of being selected.

--- vocabularydrill.js
+++ vocabularydrill.js
@@ -5,12 +5,12 @@ vocabularyDrillApp.controller('SessionCtrl', ['$scope', 'Feedback',
     function($scope, Feedback) {
         $scope.currentSection = '';
         $scope.words = [];
-        $scope.selectedWord = '';   // Selected word in the foreign language.
+        $scope.word = undefined;
         $scope.showingAnswer = false;
 
         // Note: have to use a method here, because in a direct assignment only the local scope gets changed.
         $scope.setWords = function(words) {
-            // Assign words array, but first get history of each word.
+            // Assign words array, but add history of each word.
             $scope.words = words.map(function(word) {
                 // word is a 2-tuple
                 var historyForWord = Feedback.historyForWord(word[0]);
@@ -26,10 +26,33 @@ vocabularyDrillApp.controller('SessionCtrl', ['$scope', 'Feedback',
                         }).length
                 }
             });
-        }
+            $scope.updateProbabilities();
+        };
 
-        $scope.setSelectedWord = function(word) {
-            $scope.selectedWord = word;
+        $scope.updateProbabilities = function() {
+            // Compute selection probability, which is equal to the ratio of wrong vs correct, but it is 1 if no attempt has been made yet for this word.
+            $scope.words.map(function(word) {
+                var totalAttempts = word.wrong + word.correct;
+                word.selectionProbability = totalAttempts > 0 ? (word.wrong / (word.wrong + word.correct) + 0.1) : 1;   // 0.1: always a small chance of selecting this word.
+            });
+            // Normalize PDF by summing all values...
+            var probabilitySum = $scope.words.map(function(word) {
+                    return word.selectionProbability;
+                }).reduce(function(a, b) {
+                    return a + b;
+                });
+            // then set PDF to selection probability divided by the sum of individual probabilities to normalize the PDF.
+            // The CDF is the cumulative sum of the PDF.
+            var currentCDF = 0.0;
+            $scope.words.map(function(word) {
+                word.pdf = word.selectionProbability / probabilitySum;
+                word.cdf = currentCDF;
+                currentCDF += word.pdf;
+            });
+        };
+
+        $scope.setWord = function(word) {
+            $scope.word = word;
         };
 
         $scope.needNewQuestion = function() {
@@ -60,23 +83,31 @@ vocabularyDrillApp.controller('VocabularyCtrl', ['$scope', 'Vocabulary',
 
 vocabularyDrillApp.controller('FlashCardCtrl', ['$scope', '$timeout',
     function FlashCardCtrl($scope, $timeout) {
-        $scope.word = [];
         $scope.question = 0;
         $scope.show = 0;
         $scope.showingQuestion = true;
 
         $scope.hasWord = function() {
-            return $scope.word !== undefined && $scope.word.length > 0;
+            return $scope.word !== undefined && $scope.word.word.length > 0;
         };
+
         $scope.newQuestion = function() {
-            var index = Math.floor(Math.random() * $scope.words.length);
             $scope.showingQuestion = true;
             $scope.setShowingAnswer(false);
-            $scope.word = $scope.words[index].word;
+            // Take a sample of the distribution formed by the word statistics.
+            // The sample is taken by taking a random number between 0 and 1, then finding the highest word index for which the CDF is smaller than this value.
+            var randomCDF = Math.random();   // Random value between 0 and 1.
+            var selectedWord;
+            // Find word with highest CDF that is lower than the random CDF.
+            $scope.words.map(function(word) {
+                if (word.cdf < randomCDF && (!selectedWord || word.cdf > selectedWord.cdf)) {
+                    selectedWord = word;
+                }
+            });
+            $scope.setWord(selectedWord);
             $scope.question = Math.random() < 0.5 ? 0 : 1;
             $scope.show = $scope.question;
-            $scope.setSelectedWord($scope.word[0]); // 0: foreign language.
-            $scope.$broadcast('changedword', $scope.word[$scope.show]);
+            $scope.$broadcast('changedword', $scope.word.word[$scope.show]);
         };
 
         $scope.pressedWord = function() {
@@ -84,7 +115,7 @@ vocabularyDrillApp.controller('FlashCardCtrl', ['$scope', '$timeout',
                 // Show answer.
                 $scope.showingQuestion = false;
                 $scope.show = 1 - $scope.show;
-                $scope.$emit('changedword', $scope.word[$scope.show]);
+                $scope.$emit('changedword', $scope.word.word[$scope.show]);
                 $scope.setShowingAnswer(true);
             }
         };
@@ -107,7 +138,13 @@ vocabularyDrillApp.controller('FeedbackCtrl', ['$scope', 'Feedback',
         $scope.feedback = function(wasCorrect) {
             $scope.enabled = false;
             $scope.$emit('feedback-recorded');
-            Feedback.recordFeedback($scope.selectedWord, wasCorrect);
+            Feedback.recordFeedback($scope.word.word[0], wasCorrect);
+            if (wasCorrect) {
+                $scope.word.correct += 1;
+            } else {
+                $scope.word.wrong += 1;
+            }
+            $scope.updateProbabilities();
         }
         $scope.$watch('showingAnswer', function(showingAnswer) {
             if (showingAnswer) {
